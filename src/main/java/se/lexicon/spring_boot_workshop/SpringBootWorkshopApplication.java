52e06c5269d2e41755d6427971fa3fbea1767b3e
azadeh1984 <azadeh_n1984@yahoo.com>
2023-01-24 14:06:34 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/spring_boot_workshop/SpringBootWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_boot_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/spring_boot_workshop/SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_boot_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
