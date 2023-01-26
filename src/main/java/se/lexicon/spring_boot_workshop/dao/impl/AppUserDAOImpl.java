30dff3c57add96a9bb5b0675966bd4c880b3ae4c
azadeh1984 <azadeh_n1984@yahoo.com>
2023-01-26 11:13:18 +0100

    part 1 & 2

--- src/main/java/se/lexicon/spring_boot_workshop/dao/impl/AppUserDAOImpl.java
+++ src/main/java/se/lexicon/spring_boot_workshop/dao/impl/AppUserDAOImpl.java
@@ -15,30 +15,34 @@ public class AppUserDAOImpl implements AppUserDAO {
     @Override
     @Transactional(readOnly = true)
     public AppUser findById(int appUserId) {
-        return null;
+        return entityManager.find(AppUser.class, appUserId);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Collection<AppUser> findAll() {
-        return null;
+        return entityManager.createQuery("select a from AppUser a").getResultList();
     }
 
     @Override
     @Transactional
     public AppUser creat(AppUser appUser) {
-        return null;
+       entityManager.persist(appUser);
+       return appUser;
     }
 
     @Override
     @Transactional
     public AppUser update(AppUser appUser) {
-        return null;
+        entityManager.merge(appUser);
+        return appUser;
     }
 
     @Override
     @Transactional
     public void delete(int appUserId) {
+        AppUser result = entityManager.find(AppUser.class,appUserId);
+        entityManager.remove(result);
 
     }
 }
